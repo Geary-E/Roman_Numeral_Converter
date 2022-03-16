b9386c3f43b8211eb10fddb18382fad2f097826f
Geary-E <geary16.erua@gmail.com>
2022-03-15 23:54:29 -0500

    Made updates on the JS file

--- converter.js
+++ converter.js
@@ -1,6 +1,25 @@
 let inputOne = document.getElementById("number");
 let outputOne = document.getElementById("convert");
 
-function convert() {
 
-}
+let obj = {
+           1: "I",
+           5: "V", 
+           10: "X", 
+           50: "L", 
+           100: "C"
+        };
+
+           
+function convert() {
+        if(inputOne.value == 8) {
+            outputOne.value = 'VIII';
+        }
+
+        for(const key in obj) {
+            if(inputOne.value >= key) {
+                outputOne.value = obj[key];
+            }
+        }
+} 
+
